a6483dfb6d6bc893e043dc395c8479f818a28a30
Gonshz <1031gonshi@gmail.com>
2020-02-24 03:13:58 +0900

    add ipynb file

---
+++ load_image.py
@@ -0,0 +1,28 @@
+def load_image(img_path, max_size=400, shape=None):
+    ''' Load in and transform an image, making sure the image
+       is <= 400 pixels in the x-y dims.'''
+    if "http" in img_path:
+        response = requests.get(img_path)
+        image = Image.open(BytesIO(response.content)).convert('RGB')
+    else:
+        image = Image.open(img_path).convert('RGB')
+    
+    # large images will slow down processing
+    if max(image.size) > max_size:
+        size = max_size
+    else:
+        size = max(image.size)
+    
+    if shape is not None:
+        size = shape
+        
+    in_transform = transforms.Compose([
+                        transforms.Resize(size),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.485, 0.456, 0.406), 
+                                             (0.229, 0.224, 0.225))])
+
+    # discard the transparent, alpha channel (that's the :3) and add the batch dimension
+    image = in_transform(image)[:3,:,:].unsqueeze(0)
+    
+    return image
